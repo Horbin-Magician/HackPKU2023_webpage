6d08f5a04e76af2363ab150e4ffe59f1f9f22373
Horbin <2240045751@qq.com>
2023-04-08 10:42:49 +0800

    Update: download multi-files.

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -26,18 +26,48 @@ v-col.d-flex.flex-column.align-center
         :color='styles["decoration-color"]'
         dark
         large
-        @click='downloadHandler'
+        @click='downloadHandler("赛题")'
       )
         v-icon(left) mdi-download
         | 赛题下载
-      v-dialog(v-model='downloadDialog' max-width='300')
-        v-card
-          v-card-title 错误
-          v-card-text {{ downloadDialogErrInfo }}
     v-col
-      v-btn(:color='styles["decoration-color"]' dark large href='#')
-        v-icon(left) mdi-video
-        | 企业技术答疑回放
+      v-btn(
+        :color='styles["decoration-color"]'
+        dark
+        large
+        @click='downloadHandler("技术支持")'
+      )
+        v-icon(left) mdi-download
+        | 技术支持
+    v-col
+      v-btn(
+        :color='styles["decoration-color"]'
+        dark
+        large
+        @click='downloadHandler("其他资料")'
+      )
+        v-icon(left) mdi-download
+        | 其他资料
+    v-dialog(v-model='errorDialog' max-width='400')
+      v-card
+        v-card-title 错误
+        v-card-text {{ errorDialogErrInfo }}
+    v-dialog(v-model='downloadDialog' max-width='400')
+      v-card
+        v-card-title {{ downloadDialogTitle }}
+        v-divider
+        v-card-text
+          v-list
+            v-list-item(v-for='file in files' :key='file.filename')
+              v-row 
+                v-col(cols='9') {{ file.filename }}
+                v-col
+                  v-btn(
+                    :color='styles["decoration-color"]'
+                    dark
+                    small
+                    :href='file.url'
+                  ) 下载
 </template>
 
 <script>
@@ -45,30 +75,46 @@ import styles from '../scss/export.scss'
 
 export default {
   data() {
-    return { styles, downloadDialog: false, downloadDialogErrInfo: '' }
+    return {
+      styles,
+      files: [],
+      errorDialog: false,
+      errorDialogErrInfo: '',
+      downloadDialog: false,
+      downloadDialogTitle: '',
+    }
   },
   methods: {
-    downloadHandler() {
-      fetch('https://hackpku.obs.cn-north-4.myhuaweicloud.com')
+    downloadHandler(flag = '赛题') {
+      let root_url = 'https://hackpku.obs.cn-north-4.myhuaweicloud.com/'
+      fetch(root_url + '?prefix=' + flag + '/')
         .then((response) => response.text())
         .then((str) => new DOMParser().parseFromString(str, 'text/xml'))
         .then((data) => {
+          this.files = []
           let files = data.getElementsByTagName('Contents')
           for (let i = 0, len = files.length; i < len; i++) {
             let fileName = files[i].getElementsByTagName('Key')[0].innerHTML
-            if (fileName.indexOf('赛题') == 0) {
-              window.open(
-                'https://hackpku.obs.cn-north-4.myhuaweicloud.com/' + fileName
-              )
-              return
+            let size = files[i].getElementsByTagName('Size')[0].innerHTML
+            let url = root_url + fileName
+            fileName = fileName.substring(flag.length + 1)
+            if (fileName.length > 0) {
+              let file = { filename: fileName, size: size, url: url }
+              this.files.push(file)
             }
           }
-          this.downloadDialogErrInfo = '比赛尚未开始，请耐心等待！'
-          this.downloadDialog = true
+          if (this.files.length == 0) {
+            this.errorDialogErrInfo =
+              '比赛尚未开始或该文件尚未准备好，请耐心等待！'
+            this.errorDialog = true
+          } else {
+            this.downloadDialogTitle = flag + '下载'
+            this.downloadDialog = true
+          }
         })
         .catch(() => {
-          this.downloadDialogErrInfo = '未知错误，请重试或联系工作人员！'
-          this.downloadDialog = true
+          this.errorDialogErrInfo = '未知错误，请重试或联系工作人员！'
+          this.errorDialog = true
         })
     },
   },
